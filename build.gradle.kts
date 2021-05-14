6b817a1a5d73ba08fca4a2c82cbee917102a5c2d
denispetrik <denthegenius@gmail.com>
2021-05-14 17:04:48 +0300

    project initialization

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.32"
+}
+
+group = "dev.denispetrik"
+version = "0.0.1-SNAPSHOT"
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+repositories {
+    mavenCentral()
+}
