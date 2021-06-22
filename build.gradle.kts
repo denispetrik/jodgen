bf92c1bc4e896c368783501efebf0d9a3f57d470
denispetrik <denthegenius@gmail.com>
2021-06-22 21:35:58 +0300

    gradle publishing plugin is applied

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,22 @@
 plugins {
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.5.10"
     `java-gradle-plugin`
     `maven-publish`
+    id("com.gradle.plugin-publish") version "0.15.0"
+}
+
+pluginBundle {
+    website = "https://github.com/denispetrik/jodgen"
+    vcsUrl = "https://github.com/denispetrik/jodgen.git"
+    tags = listOf("jooq", "dsl", "generator")
 }
 
 gradlePlugin {
     plugins {
         create("jodgen") {
             id = "dev.denispetrik.jodgen"
+            displayName = "Jooq dsl generator"
+            description = "A plugin for jooq dsl class generation"
             implementationClass = "dev.denispetrik.jodgen.JodgenPlugin"
         }
     }
@@ -22,13 +31,12 @@ publishing {
 }
 
 group = "dev.denispetrik"
-version = "0.1.0-SNAPSHOT"
+version = "0.1.0"
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.testcontainers:postgresql:1.15.3")
     implementation("org.flywaydb:flyway-core:7.9.0")
-    implementation("org.jooq:jooq-meta:3.14.9")
     implementation("org.jooq:jooq-codegen:3.14.9")
     implementation("org.postgresql:postgresql:42.2.20.jre7")
 }
